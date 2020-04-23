7eac9121ea211fdc636ec7a83e3a7dcb9a884208
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-23 14:09:15 -0700

    added the first empty reducer

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,14 @@
+const initialstate ={
+    data: [],
+    isFetching: false,
+    error: ""
+
+
+}
+
+export const reducer =( state = initialstate, action) =>{
+    switch(action.type){
+        default:
+            return state
+    }
+}
